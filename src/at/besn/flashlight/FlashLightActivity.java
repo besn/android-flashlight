2cb3eeb444b0e82760e07e27e9649b26afdd8aa4
andi mery <besn@besn.at>
2012-07-14 21:14:17 +0200

    Some minor optimizations

--- src/at/besn/flashlight/FlashLightActivity.java
+++ src/at/besn/flashlight/FlashLightActivity.java
@@ -2,46 +2,90 @@ package at.besn.flashlight;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Process;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
-import at.besn.flashlight.R;
 
 public class FlashLightActivity extends Activity {
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-		setContentView(R.layout.main);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.lampenmenu, menu);
-		return true;
-	}
+  public SharedPreferences sharedPrefs;
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle item selection
-		switch (item.getItemId()) {
-		case R.id.quit:
-			Process.killProcess(Process.myPid());
-			// finish();
-			return true;
-		case R.id.about:
-			Intent myIntent = new Intent(FlashLightActivity.this, AboutScreenActivity.class);
-			FlashLightActivity.this.startActivity(myIntent);
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    // Hide the window title,
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    // set the window to fullscreen...
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    // ...and keep the screen from fading to black
+    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+    // Make the screen full bright for this activity.
+    WindowManager.LayoutParams lp = getWindow().getAttributes();
+    lp.screenBrightness = 1.0f;
+    getWindow().setAttributes(lp);
+
+    setContentView(R.layout.main);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.activity_flash_light, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle item selection
+    switch (item.getItemId()) {
+      case R.id.menu_about:
+        startActivity(new Intent(FlashLightActivity.this, FlashLightAboutScreenActivity.class));
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
+  @Override
+  public void onRestart() {
+    Log.i(getClass().getName(), "onRestart");
+    super.onRestart();
+  }
+
+  @Override
+  public void onStart() {
+    Log.i(getClass().getName(), "onStart");
+    super.onStart();
+  }
+
+  @Override
+  public void onResume() {
+    Log.i(getClass().getName(), "onResume");
+    super.onResume();
+  }
+
+  @Override
+  public void onPause() {
+    Log.i(getClass().getName(), "onPause");
+    super.onPause();
+  }
+
+  @Override
+  public void onStop() {
+    Log.i(getClass().getName(), "onStop");
+    super.onStop();
+  }
+
+  @Override
+  public void onDestroy() {
+    Log.i(getClass().getName(), "onDestroy");
+    super.onDestroy();
+  }
 }
