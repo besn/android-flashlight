9398a4aca89095b8cbc9d3e08ee077279ab89852
Andi Mery <besn@besn.at>
2014-10-12 16:11:55 +0200

    Reformatted the code

--- app/src/main/java/at/besn/flashlight/FlashLightAboutScreenActivity.java
+++ app/src/main/java/at/besn/flashlight/FlashLightAboutScreenActivity.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.view.WindowManager;
 
 public class FlashLightAboutScreenActivity extends Activity {
-  /** Called when the activity is first created. */
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    setContentView(R.layout.about);
-  }
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.about);
+    }
 }
--- app/src/main/java/at/besn/flashlight/FlashLightActivity.java
+++ app/src/main/java/at/besn/flashlight/FlashLightActivity.java
@@ -16,85 +16,85 @@ import android.widget.RelativeLayout;
 
 public class FlashLightActivity extends Activity {
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    // Hide the window title,
-    requestWindowFeature(Window.FEATURE_NO_TITLE);
-    // set the window to fullscreen...
-    getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    // ...and keep the screen from fading to black
-    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        // Hide the window title,
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // set the window to fullscreen...
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // ...and keep the screen from fading to black
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
-    // Get the shared preferences
-    SharedPreferences sharedPrefs = this.getSharedPreferences("at.besn.flashlight", Context.MODE_PRIVATE);
+        // Get the shared preferences
+        SharedPreferences sharedPrefs = this.getSharedPreferences("at.besn.flashlight", Context.MODE_PRIVATE);
 
-    // Make the screen full bright for this activity.
-    WindowManager.LayoutParams lp = getWindow().getAttributes();
-    lp.screenBrightness = sharedPrefs.getFloat("at.besn.flashlight.brightness", 1.0f);
-    getWindow().setAttributes(lp);
+        // Make the screen full bright for this activity.
+        WindowManager.LayoutParams lp = getWindow().getAttributes();
+        lp.screenBrightness = sharedPrefs.getFloat("at.besn.flashlight.brightness", 1.0f);
+        getWindow().setAttributes(lp);
 
-    setContentView(R.layout.activity_flashlight);
+        setContentView(R.layout.activity_flashlight);
 
-    // Set the flashlights color
-    RelativeLayout flashLightField = (RelativeLayout) this.findViewById(R.id.flashLightField);
-    flashLightField.setBackgroundColor(Color.parseColor(sharedPrefs.getString("at.besn.flashlight.color", "#FFFFFF")));
-    flashLightField.invalidate();
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    MenuInflater inflater = getMenuInflater();
-    inflater.inflate(R.menu.activity_flash_light, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle item selection
-    switch (item.getItemId()) {
-      case R.id.menu_about:
-        startActivity(new Intent(FlashLightActivity.this, FlashLightAboutScreenActivity.class));
-        return true;
-      default:
-        return super.onOptionsItemSelected(item);
+        // Set the flashlights color
+        RelativeLayout flashLightField = (RelativeLayout) this.findViewById(R.id.flashLightField);
+        flashLightField.setBackgroundColor(Color.parseColor(sharedPrefs.getString("at.besn.flashlight.color", "#FFFFFF")));
+        flashLightField.invalidate();
     }
-  }
 
-  @Override
-  public void onRestart() {
-    Log.i(getClass().getName(), "onRestart");
-    super.onRestart();
-  }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_flash_light, menu);
+        return true;
+    }
 
-  @Override
-  public void onStart() {
-    Log.i(getClass().getName(), "onStart");
-    super.onStart();
-  }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_about:
+                startActivity(new Intent(FlashLightActivity.this, FlashLightAboutScreenActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-  @Override
-  public void onResume() {
-    Log.i(getClass().getName(), "onResume");
-    super.onResume();
-  }
+    @Override
+    public void onRestart() {
+        Log.i(getClass().getName(), "onRestart");
+        super.onRestart();
+    }
 
-  @Override
-  public void onPause() {
-    Log.i(getClass().getName(), "onPause");
-    super.onPause();
-  }
+    @Override
+    public void onStart() {
+        Log.i(getClass().getName(), "onStart");
+        super.onStart();
+    }
 
-  @Override
-  public void onStop() {
-    Log.i(getClass().getName(), "onStop");
-    super.onStop();
-  }
+    @Override
+    public void onResume() {
+        Log.i(getClass().getName(), "onResume");
+        super.onResume();
+    }
 
-  @Override
-  public void onDestroy() {
-    Log.i(getClass().getName(), "onDestroy");
-    super.onDestroy();
-  }
+    @Override
+    public void onPause() {
+        Log.i(getClass().getName(), "onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.i(getClass().getName(), "onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i(getClass().getName(), "onDestroy");
+        super.onDestroy();
+    }
 }
